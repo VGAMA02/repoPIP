b7b3244b72ed656f7e2c01ada78ebd767f7aa0ed
Gama <gamagama90@hotmail.com>
2025-04-22 14:00:44 -0600

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repopiip",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="VGAMA",                         # Tu nombre
+    author_email="gamgama90@hotmail.com",                 # Tu correo electrónico
+    url="https://github.com/vgama02/repopip",     # URL del proyecto
+)
